f8e6c66ec984564cfff2233af4ad1308e034df64
Aidan1223f <aidan.nt76@gmail.com>
2026-01-05 17:47:12 -0800

    the provision script now injects the ENV file

--- scripts/provision-key.js
+++ scripts/provision-key.js
@@ -11,6 +11,8 @@ const fs = require('fs');
 const path = require('path');
 const https = require('https');
 const http = require('http');
+require('dotenv').config();
+
 
 // Colors for console output
 const colors = {
