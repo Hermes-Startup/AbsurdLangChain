eb8a6699d01011fc35ca4fc44613e6ee17e0c53e
Aaron Soekiatno <aaronjsoekiatno@gmail.com>
2026-01-06 21:40:48 -0800

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 const publicSans = Public_Sans({ subsets: ["latin"] });
-
+//test
 const Logo = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
